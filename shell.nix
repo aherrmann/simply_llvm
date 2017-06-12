9e2b624aa92d4d943471b1cc62a343925e3c989c
Andreas Herrmann <andreash87@gmx.ch>
2017-06-12 22:14:39 +0200

    nix-shell formatting

--- shell.nix
+++ shell.nix
@@ -13,13 +13,13 @@ let
       GenericPretty
     ]);
 in
-  pkgs.stdenv.mkDerivation {
-    name = "simply-llvm-env";
-    buildInputs = [
-      cabal-install ghc llvm_4 clang_4
-    ];
-    shellHook = ''
-      eval $(egrep ^export ${ghc}/bin/ghc)
-      PS1='nix:\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]$(__git_ps1 "(%s)")\$ '
-    '';
-  }
+pkgs.stdenv.mkDerivation {
+  name = "simply-llvm-env";
+  buildInputs = [
+    cabal-install ghc llvm_4 clang_4
+  ];
+  shellHook = ''
+    eval $(egrep ^export ${ghc}/bin/ghc)
+    PS1='nix:\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]$(__git_ps1 "(%s)")\$ '
+  '';
+}
